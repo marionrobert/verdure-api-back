756fad13652fb0b92f13df1fed73e447e6f45db3
Marion Robert <mrobert148@gmail.com>
2023-07-03 16:49:42 +0200

    order Model ok

--- models/OrderModel.js
+++ models/OrderModel.js
@@ -6,48 +6,87 @@ module.exports = (_db)=>{
 class OrderModel {
     //validation d'une commande
     static saveOneOrder(user_id, totalAmount){
-        //le status sera "not payed" par défault
-
+      //le status sera "not payed" par défault
+      let sql = "INSERT INTO orders (user_id, totalAmount, creationTimestamp, status) VALUES(?, ?, ?, ?)"
+      return db.query(sql, [user_id, totalAmount, new Date(), "not payed"])
+      .then((res)=>{
+        return res
+      })
+      .catch((err)=>{
+        return err
+      })
 
     }
 
     //sauvegarde d'un orderDetail
-    static saveOneOrderDetail(order_id, plant){
-
-
-
+    static saveOneOrderDetail(order_id, plant_id){
+      let sql = "INSERT INTO orderdetails (order_id, plant_id, quantity, total) VALUES(?, ?, ?, ?)"
+      return db.query(sql, [order_id, plant_id, quantity, total])
+      .then((res)=>{
+        return res
+      })
+      .catch((err)=>{
+        err
+      })
     }
 
     //modification du montant total
     static updateTotalAmount(order_id, totalAmount){
-
-
-
+      let sql = "UPDATE orders SET totalAmount=? WHERE id=?"
+      return db.query(sql, [totalAmount, order_id])
+      .then((res)=>{
+        return res
+      })
+      .catch((err)=>{
+        return err
+      })
     }
 
     //récupération d'une commande en fonction d'un id
     static getOneOrder(id){
-
-
-
+      let sql = "SELECT * FROM orders WHERE id = ?"
+      return db.query(sql, [id])
+      .then((res)=>{
+        return res
+      })
+      .catch((err)=>{
+        return err
+      })
     }
 
     //modification d'un status de commande
-    static updateStatus(orderId, status){
-
-
-
+    static updateStatus(order_id, status){
+      let sql = "UPDATE orders SET status = ? WHERE id = ?"
+      return db.query(sql, [status, order_id])
+      .then((res)=>{
+        return res
+      })
+      .catch((err)=>{
+        return err
+      })
     }
 
     //récupération de toutes les commandes
     static getAllOrders(){
-
-
+      let sql = "SELECT * FROM orders"
+      return db.query(sql)
+      .then((res)=>{
+        return res
+      })
+      .catch((err)=>{
+        return err
+      })
     }
 
     //récupération des détails d'une commande
-    static getAllDetails(orderId){
-
-
+    static getAllDetails(order_id){
+      let sql = "SELECT * FROM ordersdetails WHERE order_id = ?"
+      return db.query(sql, [order_id])
+      .then((res)=>{
+        return res
+      })
+      .catch((err)=>{
+        return err
+      })
     }
 }
