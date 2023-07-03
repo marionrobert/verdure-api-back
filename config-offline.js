40f155f36e893c78ee5c0615e7a35efaf95b3bd6
Marion Robert <mrobert148@gmail.com>
2023-07-03 12:15:49 +0200

    ready to push after ignoring config-offline.js

--- config-offline.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    db: {
-        host: "db.3wa.io",
-        database: "marionrobert_verdure",
-        user: "marionrobert",
-        password: "1a666de5d2caaa86fce86f15cc4018f4!!!!"
-        //port: 8889 - pour MAC
-    }
-}
